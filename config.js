3606ff83bf0b184a868c17178f35343cfc000ed1
DEVEL-LK <gimshandilisha578@gmail.com>
2025-11-13 09:56:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝚀𝚞𝚎𝚎𝚗_𝙰𝚑𝚒𝚗𝚜𝚊-𝚞𝚒𝚍~SNNzQQSQ#KuZ3Bv8W1OVocITD5ZATLRZWxTax0CvOKBcJsV_LDUc",
+SESSION_ID: process.env.SESSION_ID || "𝚀𝚞𝚎𝚎𝚗_𝙰𝚑𝚒𝚗𝚜𝚊-𝚞𝚒𝚍~GU9EVbLK#qknOangRU1oP092HxNzm-2F84XwJ4xjdudLAI5Jq_ME",
 MONGODB: process.env.MONGODB || "mongodb://mongo:tRCemTxbjefxwnptSDmKfzJbzReKVkNQ@crossover.proxy.rlwy.net:51796",
 };
